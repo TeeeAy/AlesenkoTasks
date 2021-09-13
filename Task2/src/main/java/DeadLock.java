2a42e55fde595b44309bc76883fd2be9d865cd06
Alesenko Antonina <antonina.alesenko@gmail.com>
2021-09-13 03:31:53 +0300

    Task2 updated

--- Task2/src/main/java/DeadLock.java
+++ Task2/src/main/java/DeadLock.java
@@ -3,46 +3,35 @@ import java.util.logging.Logger;
 public class DeadLock {
 
     private static final Logger LOGGER = Logger.getLogger(DeadLock.class.getName());
-    private final Resource resource1 = new Resource("resource №1");
-    private final Resource resource2 = new Resource("resource №2");
 
 
-    public void workWithResources(){
-        synchronized (resource1){
-            LOGGER.info("Thread '"+ Thread.currentThread().getName()+"' got "+resource1);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            LOGGER.info("Thread '"+ Thread.currentThread().getName()+"' is waiting for "+resource2);
+    public void workWithResources(final Resource resource1, final Resource resource2) {
+        synchronized (resource1) {
+            LOGGER.info("Thread '" + Thread.currentThread().getName() + "' got " + resource1);
+            sleep(1000);
+            LOGGER.info("Thread '" + Thread.currentThread().getName() + "' is waiting for " + resource2);
             synchronized (resource2) {
-                LOGGER.info("Thread '"+ Thread.currentThread().getName()+"' has got all" +
-                        " necessary resources and finished work");
-            }
-        }
-    }
-
-    public void workWithResourcesInReverseOrder(){
-        synchronized (resource2){
-            LOGGER.info("Thread '"+ Thread.currentThread().getName()+"' got "+resource2);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            LOGGER.info("Thread '"+ Thread.currentThread().getName()+"' is waiting for "+resource1);
-            synchronized (resource1) {
-                LOGGER.info("Thread '"+ Thread.currentThread().getName()+"' has got all" +
+                LOGGER.info("Thread '" + Thread.currentThread().getName() + "' has got all" +
                         " necessary resources and finished work");
             }
         }
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        final Resource resource1 = new Resource("resource №1");
+        final Resource resource2 = new Resource("resource №2");
         DeadLock deadLock = new DeadLock();
-        new Thread(deadLock::workWithResources).start();
-        new Thread(deadLock::workWithResourcesInReverseOrder).start();
+        new Thread(() -> deadLock.workWithResources(resource1, resource2)).start();
+        new Thread(() -> deadLock.workWithResources(resource2, resource1)).start();
     }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- Task2/src/main/java/LiveLock.java
+++ Task2/src/main/java/LiveLock.java
@@ -2,42 +2,48 @@ import java.util.logging.Logger;
 
 public class LiveLock {
 
-    private static final Logger LOGGER = Logger.getLogger(DeadLock.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(LiveLock.class.getName());
 
     public static void workWithResourcesIfAvailable(Resource resource1, Resource resource2) {
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            resource1.setAvailable(false);
-            LOGGER.info(Thread.currentThread().getName() + " is waiting for " + resource2 +
-                    " to become available");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if (resource2.isAvailable()) {
-                LOGGER.info("Got " + resource2);
-                resource1.setAvailable(true);
-                break;
-            } else {
-                LOGGER.info(Thread.currentThread().getName() + " is setting " + resource1 +
-                        " available");
-                resource1.setAvailable(true);
+        while (resource1.isAvailable()) {
+            synchronized (resource1) {
+                resource1.setAvailable(false);
+                LOGGER.info(Thread.currentThread().getName() + " is checking whether "
+                        + resource2 + " is available");
+                DeadLock.sleep(100);
+                if (resource2.isAvailable()) {
+                    synchronized (resource2) {
+                        LOGGER.info(Thread.currentThread().getName() + " HAS GOT " + resource2);
+                        resource2.notify();
+                        break;
+                    }
+                } else {
+                    LOGGER.info(Thread.currentThread().getName() + " is setting " + resource1 +
+                            " available");
+                    resource1.setAvailable(true);
+                    wait(resource1, 7);
+                }
             }
         }
+        resource1.setAvailable(true);
     }
 
 
     public static void main(String[] args) {
         Resource resource1 = new Resource("resource №1");
         Resource resource2 = new Resource("resource №2");
+        resource1.setAvailable(true);
+        resource2.setAvailable(true);
         new Thread(() -> workWithResourcesIfAvailable(resource1, resource2)).start();
         new Thread(() -> workWithResourcesIfAvailable(resource2, resource1)).start();
+    }
+
+    public static void wait(Object obj, int millis) {
+        try {
+            obj.wait(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
